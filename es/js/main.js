c5af092f6f8c0f7aceeb498c82d64843da3ab874
dimitrijs22 <dimitrij222@hotmail.com>
2024-04-17 20:32:11 +0300

    Review-Site

---
+++ es/js/main.js
@@ -0,0 +1,6 @@
+// Replace the 75 with the rating value (0 to 100)
+const ratingValue = 75;
+const ratingFill = document.querySelector(".rating-fill");
+ratingFill.style.width = ratingValue + "%";
+
+/* NEWS API */
